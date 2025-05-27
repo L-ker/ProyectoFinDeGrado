c94c450738a3b6d5fcf7952002bbe008473b240a
Luker <lucasperezgar@gmail.com>
2025-05-28 01:02:37 +0200

    cambio header para volver al home

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <header class="hidden md:flex h-15v bg-rojoClaro flex flex-row justify-between px-3 py-1 items-center">
-    <img class="w-28 md:w-36 lg:w-44 max-h-[90%] h-auto" src="{{ asset('images/logo.png') }}" alt="{{__('logo')}}">
+    <a href="{{ route('home') }}"><img class="w-28 md:w-36 lg:w-44 max-h-[90%] h-auto" src="{{ asset('images/logo.png') }}" alt="{{__('logo')}}"></a>
     <img class="w-28 md:w-36 lg:w-44 max-h-[90%] h-auto" src="{{ asset('images/corona.png') }}" alt="{{__('logo')}}">
     <div>
         @auth
